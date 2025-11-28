57f68a5d5dcb4d001a17f6a08067a47773fc6be8
xDaoud <moshdawd@gmail.com>
2025-11-28 20:53:15 +0200

    feat: add WebSocket chat controller

--- src/main/java/com/xDaoud/realtime_chat_app/controller/ChatController.java
+++ src/main/java/com/xDaoud/realtime_chat_app/controller/ChatController.java
@@ -1,4 +1,16 @@
 package com.xDaoud.realtime_chat_app.controller;
 
+import com.xDaoud.realtime_chat_app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/char.sendMessage")
+    @SendTo("/public/topic")
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
 }
