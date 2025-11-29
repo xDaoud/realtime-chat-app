f3145783005c95282a840e130c88f4117a311cac
xDaoud <moshdawd@gmail.com>
2025-11-29 21:28:39 +0200

    feat: add chat message types and user join handling

--- src/main/java/com/xDaoud/realtime_chat_app/controller/ChatController.java
+++ src/main/java/com/xDaoud/realtime_chat_app/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.xDaoud.realtime_chat_app.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChatController {
@@ -11,6 +12,13 @@ public class ChatController {
     @MessageMapping("/char.sendMessage")
     @SendTo("/public/topic")
     public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @RequestMapping("/chat.addUser")
+    @SendTo("/public/topic")
+    public ChatMessage addUser(ChatMessage chatMessage) {
+        chatMessage.setMessageType(ChatMessage.MessageType.JOIN);
         return chatMessage;
     }
 }
--- src/main/java/com/xDaoud/realtime_chat_app/model/ChatMessage.java
+++ src/main/java/com/xDaoud/realtime_chat_app/model/ChatMessage.java
@@ -3,11 +3,18 @@ package com.xDaoud.realtime_chat_app.model;
 public class ChatMessage {
     private String sender;
     private String content;
+    private MessageType messageType;
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+
     public ChatMessage() {
     }
-    public ChatMessage(String sender, String content) {
+    public ChatMessage(String sender, String content, MessageType type) {
         this.sender = sender;
         this.content = content;
+        this.messageType = type;
     }
     public String getSender() {
         return sender;
@@ -20,5 +27,12 @@ public class ChatMessage {
     }
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
     }
 }
