5bdd6241ef43a73bea927c1669b876c5c9c8e39c
xDaoud <moshdawd@gmail.com>
2025-12-08 23:19:23 +0200

    refacter: add save method to the ChatMessageRepository

--- src/main/java/com/xDaoud/realtime_chat_app/repository/ChatMessageRepository.java
+++ src/main/java/com/xDaoud/realtime_chat_app/repository/ChatMessageRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByChatRoomIdOrderById(Long chatRoomId);
+    ChatMessage save(ChatMessage chatMessage);
 }
